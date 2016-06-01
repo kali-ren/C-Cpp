1ec7b8a5adaefa3b510df8a550540fec5ba4bd8e
kali-ren <renan@insert.uece.br>
2016-06-01 18:31:26 -0300

    mah oi

--- hashlista.c
+++ hashlista.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define size 5
 typedef struct no{
 	int id,flag;
 	struct no *prox;
@@ -12,13 +13,13 @@ typedef struct vetor{
 	
 }vetor;
 
-vetor proxeneta[7];
+vetor proxeneta[size];
 
 
 void insercaomod(int i, vetor *prox){
 	int r;
-	r = i%7;
-	printf("%dmod%d=%d\n",i,7,r);
+	r = i%size;
+	printf("%dmod%d=%d\n",i,size,r);
 	no *new;
 	new=malloc(sizeof(no));
 	new->id=i;
@@ -34,7 +35,7 @@ void insercaomod(int i, vetor *prox){
 }
 int busca(int i,vetor *prox){//optimizar !
 	int r;	
-	r = i%7;
+	r = i%size;
 	no *aux;
 	aux=prox[r].noo;
 	if(aux==NULL){
@@ -42,47 +43,55 @@ int busca(int i,vetor *prox){//optimizar !
 		return 0;
 	}
 	else{
-		while(aux != NULL && aux->id != i){
-			if(aux->id == i){
-				printf("valor existe !\n");
-				return 1;
-			}
-			else{
+		while(aux != NULL && aux->id != i)
 				aux=aux->prox;
-			}
+		if(aux == NULL){
+			printf("valor nao existe");
+			return 0;
 		}
-		printf("valor nao existe\n");
-		return 0;
+		else if(aux->id == i){
+			printf("valor existe");
+			return 1;
+		}
+		else{
+			printf("valor nao existe\n");
+			return 0;
+		}	
 	}
 }
 void remocao(int i, vetor *prox){
 	int r;
 	no *ant,*p;
-	r = i%7;
+	r = i%size;
 	ant = prox[r].noo;
 	p = ant->prox;
-	if(ant->id == i && p != NULL){
-		ant->id=p->id;
-		ant->prox = p->prox;
-		free(p);
-	}
-	else if(ant->id == i && p == NULL){
-		prox[r].noo=NULL;		
-		
+	if(busca(i,prox)==0){
+		printf("%d nao existe na estrutura !!\n",i);
 	}
 	else{
-		while(p != NULL && p->id !=i ){
-			ant=p;
-			p=p->prox;
+		if(ant->id == i && p != NULL){
+			ant->id=p->id;
+			ant->prox = p->prox;
+			free(p);
 		}
-		ant->prox=p->prox;
-		free(p);
+		else if(ant->id == i && p == NULL){
+			prox[r].noo=NULL;		
+		
+		}
+		else{
+			while(p != NULL && p->id !=i ){
+				ant=p;
+				p=p->prox;
+			}
+			ant->prox=p->prox;
+			free(p);
+		}
 	}		
 }
 void whenitjustgoestoshow(vetor *prox){
 	int i;
 	no *aux;
-	for(i=0;i<7;i++){
+	for(i=0;i<size;i++){
 		printf("proxeneta[%d]",i);
 		aux = prox[i].noo;
 		while(aux != NULL){
@@ -95,18 +104,21 @@ void whenitjustgoestoshow(vetor *prox){
 
 }		
 main(){
-	int i, v[9]={0,1,85,6,36,46,89,112,44},a;
-	for(i=0;i<9;i++)
+	int i, v[20]={16,18,22,29,27,2,18,13,12,28,31,81,1,19,8,14,91,32,1,31},a;
+	for(i=0;i<20;i++)
 		insercaomod(v[i],proxeneta);
 	
 	whenitjustgoestoshow(proxeneta);
-//	remocao(36,proxeneta);
-//	remocao(85,proxeneta);
-//	remocao(1,proxeneta);
+	remocao(81,proxeneta);
+	remocao(71,proxeneta);
+	remocao(14,proxeneta);
+	remocao(22,proxeneta);
+	remocao(1,proxeneta);
 	printf("////////////////////////\n");
 	whenitjustgoestoshow(proxeneta);
-	a=busca(10,proxeneta);
-	printf(" %d\n",a);
+//	a=busca(71,proxeneta);
+//	printf(" %d\n",a);
 
 
 }
+
