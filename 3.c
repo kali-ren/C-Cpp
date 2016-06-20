f1b14912898876159e19d478c43facd309a1b903
kali-ren <renan@insert.uece.br>
2016-06-19 22:10:15 -0300

    mp

--- 3.c
+++ 3.c
@@ -2,6 +2,9 @@
 #include<stdlib.h>
 #include<time.h>
 
+#define max 100000
+
+
 typedef struct no{
 	int id;
 	struct no *prox;
@@ -12,7 +15,6 @@ void show(no *l){
 	for(p = l->prox; p != NULL; p = p->prox)
 		printf("%d\n",p->id);
 }
-
 void insert(no *pc){
 	no *p,*new,*end;
 	p=pc;
@@ -27,14 +29,44 @@ void insert(no *pc){
 		end=new;
 	}
 }
+
+void ba(int min, int intervalo, no *pc){
+	int i,x;
+	double media=0;
+	double tempoGasto;
+	clock_t tempoInicio, tempoFinal;
+	no *aux;
+	aux=pc;
+	printf("intervalo %d\n",intervalo);
+	x = min + rand() % intervalo;
+	printf("numero gerado %d\n",x);
+	tempoInicio = clock();
+	for(i=0; i<x; i++)
+		aux = aux->prox;
+	tempoFinal = clock();
+	tempoGasto = (double)(tempoFinal-tempoInicio)/(CLOCKS_PER_SEC/1000);
+	printf("%f tempo medio\n",tempoGasto);
+}	
+
 main(){
-	int i;
+/*	FILE *fp;
+	fp = fopen("saida.txt","w");
+	if(fp==NULL){
+		printf("falha\n");
+	}*/
+	int i,tam=1000,min=1;
 	no head,*c;
 	head.prox = NULL;
 	c = &head;
-	for(i=1;i<=100000;i++){
-		insert(c);}
-	show(c);
-
+	srand((unsigned)time(NULL));
+	for(i=1;i<=max;i++){
+		insert(c);
+	}
+	while(tam <= max){
+		ba(min,tam,c);
+		tam += 1000;
+		min += 1000;
+		printf("minimum %d\n",min);
+	}
 }
 
